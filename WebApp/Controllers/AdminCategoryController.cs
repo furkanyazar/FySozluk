1f36a1a037552f8d3238d3c10d8c008e87203b72
Furkan Yazar <furkanyazar9853@gmail.com>
2022-03-24 18:00:26 +0300

    Add theme

---
+++ WebApp/Controllers/AdminCategoryController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class AdminCategoryController : Controller
+    {
+        // GET: AdminCategory
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}

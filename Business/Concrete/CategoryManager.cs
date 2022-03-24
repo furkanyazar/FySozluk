668294c36440ae4f97ce7998183cfd7b94e754dd
Furkan Yazar <furkanyazar9853@gmail.com>
2022-03-24 09:31:19 +0300

    Add MVC of Category and list categories

---
+++ Business/Concrete/CategoryManager.cs
@@ -0,0 +1,33 @@
+﻿using DataAccess.Concrete;
+using DataAccess.Concrete.Repositories;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class CategoryManager
+    {
+        private Repository<Category, Context> repository = new Repository<Category, Context>();
+
+        public List<Category> GetAll()
+        {
+            return repository.GetAll();
+        }
+
+        public void Add(Category category)
+        {
+            if (category.CategoryName == "" || category.CategoryName.Length < 3 || category.CategoryName.Length > 50 || category.CategoryDescription == "")
+            {
+                // error message
+            }
+            else
+            {
+                repository.Add(category);
+            }
+        }
+    }
+}
---
+++ WebApp/Controllers/CategoryController.cs
@@ -0,0 +1,27 @@
+﻿using Business.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private CategoryManager manager = new CategoryManager();
+
+        // GET: Category
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetCategories()
+        {
+            var result = manager.GetAll();
+
+            return View(result);
+        }
+    }
+}

e212e2e1f7d8eed541226afe2b7403c4411a71a7
Furkan Yazar <furkanyazar9853@gmail.com>
2022-03-29 15:54:30 +0300

    Add new migration and template files

--- Business/Constants/Defaults.cs
+++ Business/Constants/Defaults.cs
@@ -2,6 +2,6 @@
 {
     public static class Defaults
     {
-        public static string DEFAULT_WRITER_IMAGE_URL = "/AdminLTE-3.0.4/dist/img/user2-160x160.jpg";
+        public static string DEFAULT_WRITER_IMAGE_URL = "/Images/user2-160x160.jpg";
     }
 }
---
+++ DataAccess/Migrations/202203291242114_mig_add_content_status.cs
@@ -0,0 +1,17 @@
+﻿namespace DataAccess.Migrations
+{
+    using System.Data.Entity.Migrations;
+
+    public partial class mig_add_content_status : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Contents", "ContentStatus", c => c.Boolean(nullable: false));
+        }
+
+        public override void Down()
+        {
+            DropColumn("dbo.Contents", "ContentStatus");
+        }
+    }
+}

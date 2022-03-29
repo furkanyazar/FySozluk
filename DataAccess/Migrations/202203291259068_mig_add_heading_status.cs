8ba9bd8f18300bd3fc691b89d72a286e3f2bc624
Furkan Yazar <furkanyazar9853@gmail.com>
2022-03-29 15:59:59 +0300

    Update Heading entity and add migration

---
+++ DataAccess/Migrations/202203291259068_mig_add_heading_status.cs
@@ -0,0 +1,17 @@
+﻿namespace DataAccess.Migrations
+{
+    using System.Data.Entity.Migrations;
+
+    public partial class mig_add_heading_status : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Headings", "HeadingStatus", c => c.Boolean(nullable: false));
+        }
+
+        public override void Down()
+        {
+            DropColumn("dbo.Headings", "HeadingStatus");
+        }
+    }
+}

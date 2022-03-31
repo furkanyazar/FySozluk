fcb5cad1166e39b637efcec3cea03dcd6470d321
Furkan Yazar <furkanyazar9853@gmail.com>
2022-03-31 16:05:24 +0300

    Add About CRUD operations, controller and view

---
+++ DataAccess/EntityFramework/EfAboutDal.cs
@@ -0,0 +1,11 @@
+﻿using DataAccess.Abstract;
+using DataAccess.Concrete;
+using DataAccess.Concrete.Repositories;
+using Entities.Concrete;
+
+namespace DataAccess.EntityFramework
+{
+    public class EfAboutDal : Repository<About, Context>, IAboutDal
+    {
+    }
+}
---
+++ WebApp/Controllers/AdminAboutController.cs
@@ -0,0 +1,34 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.EntityFramework;
+using Entities.Concrete;
+using System.Web.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class AdminAboutController : Controller
+    {
+        private IAboutService _aboutService = new AboutManager(new EfAboutDal());
+
+        // GET: About
+        public ActionResult Index()
+        {
+            var result = _aboutService.GetAll();
+
+            return View(result);
+        }
+
+        [HttpPost]
+        public ActionResult AddAbout(About about)
+        {
+            _aboutService.Add(about);
+
+            return RedirectToAction("Index");
+        }
+
+        public PartialViewResult AboutPartial()
+        {
+            return PartialView();
+        }
+    }
+}

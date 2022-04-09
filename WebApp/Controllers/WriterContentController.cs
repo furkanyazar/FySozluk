e1e025f49ad6ca360af3a77c801ee94426f33819
Furkan Yazar <furkanyazar9853@gmail.com>
2022-04-10 01:50:49 +0300

    Add WriterContent controller and view

---
+++ WebApp/Controllers/WriterContentController.cs
@@ -0,0 +1,52 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.EntityFramework;
+using Entities.Concrete;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class WriterContentController : Controller
+    {
+        private IContentService _contentService = new ContentManager(new EfContentDal());
+        private IHeadingService _headingService = new HeadingManager(new EfHeadingDal());
+        private IWriterService _writerService = new WriterManager(new EfWriterDal());
+
+        // GET: WriterContent
+        public ActionResult Index()
+        {
+            var result = _contentService.GetAllByWriterId(10).OrderByDescending(x => x.ContentDate).ToList();
+
+            GetForeignValues(result);
+
+            return View(result);
+        }
+
+        public ActionResult ContentsByHeading(int id)
+        {
+            var result = _contentService.GetAllByHeadingId(id).Where(x => x.WriterId == 10).OrderByDescending(x => x.ContentDate).ToList();
+
+            GetForeignValues(result);
+
+            return View(result);
+        }
+
+        public void GetForeignValues(ICollection<Content> result)
+        {
+            foreach (var item in result)
+            {
+                if (item.Heading is null)
+                {
+                    item.Heading = _headingService.GetById(item.HeadingId);
+                }
+
+                if (item.Writer is null)
+                {
+                    item.Writer = _writerService.GetById((int)item.WriterId);
+                }
+            }
+        }
+    }
+}

a8641a40f1a9dbd9ed623d2808beda8d6d744c71
Furkan Yazar <furkanyazar9853@gmail.com>
2022-04-11 19:23:26 +0300

    Add migration

---
+++ DataAccess/Migrations/202204111623131_mig_add_admin_status.cs
@@ -0,0 +1,17 @@
+﻿namespace DataAccess.Migrations
+{
+    using System.Data.Entity.Migrations;
+
+    public partial class mig_add_admin_status : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Admins", "AdminStatus", c => c.Boolean(nullable: false));
+        }
+
+        public override void Down()
+        {
+            DropColumn("dbo.Admins", "AdminStatus");
+        }
+    }
+}
